7d549f8e73f326e9597b208d59c770a34665230c
Artyom Ulanchik <artyom.u@outlook.com>
2016-07-17 19:19:15 +0300

    Pom update

---
+++ src/main/java/com/reborne/light/GreetingLightRestApp.java
@@ -0,0 +1,12 @@
+package com.reborne.light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingLightRestApp 
+{
+    public static void main( String[] args ) throws Exception {
+        SpringApplication.run(GreetingLightRestApp.class, args);
+    }
+}
