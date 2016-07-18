d37b9c033e6a7f0a535a234d7746f83109ef2d8d
Artyom Ulanchik <artyom.u@outlook.com>
2016-07-18 18:58:48 +0300

    Added Base Tests Configuration

---
+++ src/main/test/com/reborne/light/AbstractTest.java
@@ -0,0 +1,13 @@
+package com.reborne.light;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes=GreetingLightRestApp.class)
+public abstract class AbstractTest {
+	protected Logger log = LoggerFactory.getLogger(this.getClass());
+}
