9d76c5445ee5833bf0641f1f934ed90c8bcd28e1
Artyom Ulanchik <artyom.u@outlook.com>
2016-07-19 17:07:37 +0300

    Added Transaction Management

--- src/main/java/com/reborne/light/GreetingLightRestApp.java
+++ src/main/java/com/reborne/light/GreetingLightRestApp.java
@@ -3,9 +3,11 @@ package com.reborne.light;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableTransactionManagement
 public class GreetingLightRestApp 
 {
     public static void main( String[] args ) throws Exception {
--- src/main/java/com/reborne/light/service/GreetingServiceBean.java
+++ src/main/java/com/reborne/light/service/GreetingServiceBean.java
@@ -4,11 +4,14 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.reborne.light.model.Greeting;
 import com.reborne.light.repository.GreetingRepository;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 	
 	@Autowired
@@ -24,6 +27,7 @@ public class GreetingServiceBean implements GreetingService {
         return greeting;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting create(Greeting greeting) {
 		if (greeting.getId() != null) {
             return null;
@@ -31,7 +35,8 @@ public class GreetingServiceBean implements GreetingService {
         Greeting savedGreeting = greetingRepository.save(greeting);
         return savedGreeting;
 	}
-
+	
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting update(Greeting greeting) {
 		 Greeting greetingToUpdate = findOne(greeting.getId());
 	        if (greetingToUpdate == null) {
@@ -42,6 +47,7 @@ public class GreetingServiceBean implements GreetingService {
 	        return updatedGreeting;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void delete(Long id) {
 		greetingRepository.delete(id);
 	}
